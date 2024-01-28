b4bbcd3832d303ae766705e343fc3f7d1d6c27c7
Vivek Panchal <vivekpanchal135@gmail.com>
2024-01-28 22:09:07 +0530

    JSX

--- 03-layingTheFoundation/script.js
+++ 03-layingTheFoundation/script.js
@@ -1,8 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//using React createElement
+const title = React.createElement("div", { class: "title" }, [
+  React.createElement("h1", {}, "H1 tag"),
+  React.createElement("h2", {}, "H2 tag"),
+  React.createElement("h3", {}, "H3 Tag"),
+]);
+
+// using JSX
+const title2 = (
+  <div class="title">
+    <h1 key="1">H1 Tag</h1>
+    <h2 key="2">H2 Tag</h2>
+    <h3 key="3">H3 Tag</h3>
+  </div>
+);
+
+//Using Functional componants
+const code = () => <div>Hello</div>;
+
+const Title3 = () => (
+  <div className="title">
+    <h1>H1 Tag</h1>
+    <h2>H2 Tag</h2>
+    <h3>H3 Tag</h3>
+    {<code />}
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("container"));
+root.render(<Title3 />);
+
 const Head2 = function () {
-  return <h2 id="r">Namaste React</h2>;
+  return <h2 id="react">Namaste React</h2>;
 };
 
 const head = (
@@ -11,7 +42,3 @@ const head = (
     <Head2 />
   </div>
 );
-
-const root = ReactDOM.createRoot(document.getElementById("container"));
-
-root.render(head);
